b3f39abdc6745b7ff46b79e929a3eef372b09dc2
Igor Viana Sousa <igoorviannax@gmail.com>
2025-07-10 11:11:54 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -4,7 +4,7 @@ function generateNumber() {
     const min = Math.ceil(document.querySelector(".input-min").value)
     const max = Math.floor(document.querySelector(".input-max").value)
 
-    if(min > max){
+    if(min < max){
     const result = Math.floor(Math.random() * (max - min + 1)) + min;
     alert(result)
     }else{
