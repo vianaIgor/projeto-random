7c1bcbccc49ccf0f3cbeb34447d9ab67cd886061
vianaIgor <igoorviannax@gmail.com>
2025-07-10 09:54:27 -0300

    erro caso o usuario digite um valor < ou =

--- scripts.js
+++ scripts.js
@@ -1,10 +1,15 @@
+
 function generateNumber() {
 
     const min = Math.ceil(document.querySelector(".input-min").value)
     const max = Math.floor(document.querySelector(".input-max").value)
 
+    if(min > max){
     const result = Math.floor(Math.random() * (max - min + 1)) + min;
     alert(result)
+    }else{
+        alert("Favor digitar primeiro um número menor")
+    }
 }
 
 
